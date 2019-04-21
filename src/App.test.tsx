e746c620f715b64bb3f48ab1a686fb3beb554f05
mtucci <matteo.detucci_ext@scout24.com>
2019-04-21 20:10:40 +0200

    Implement failing test to check continuous integration

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,7 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it("fails on purpose to check continuous integration pipeline", () => {
+  expect(1).toEqual(2)
+});
